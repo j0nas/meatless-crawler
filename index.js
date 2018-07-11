05e72e9c67b7d64569d62578674b5bdf9df67b09
Jonas Jensen <jonas.jensen@greenbird.com>
2018-07-11 22:30:35 +0200

    husky, json spacing on output

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-const Crawler = require('crawler');
-const { writeFile } = require('jsonfile');
-const { promisify } = require('util');
-const URL = require('url');
+const Crawler = require("crawler");
+const { writeFile } = require("jsonfile");
+const { promisify } = require("util");
+const URL = require("url");
 
 const write = promisify(writeFile);
 
 let shouldWrite = true;
 const pages = [
-  'https://en.wikipedia.org/wiki/Animal_consciousness#Cambridge_Declaration_on_Consciousness',
-  'https://www.theguardian.com/news/2018/may/07/true-cost-of-eating-meat-environment-health-animal-welfare',
-  'http://www.hannejohansen.no/2017/02/12/hvorfor-veganer/',
-  'http://www.hannejohansen.no',
-  'https://forskning.no/2018/05/kjottreduksjon-kan-radikalt-oke-norsk-selvforsyning-av-mat/produsert-og-finansiert-av/nordlandsforskning',
-  'https://forskning.no/mat-landbruk/2018/03/hvis-alle-blir-veganere-vil-verden-fa-mye-mer-mat-kan-lose-matkrise-ved-bytte',
-  'http://waterfootprint.org/en/water-footprint/product-water-footprint/water-footprint-crop-and-animal-products/',
-  'https://www.sciencedirect.com/science/article/pii/S2212371713000024',
-  'http://www.oecd.org/agriculture/water-use-in-agriculture.htm',
-  'https://www.researchgate.net/publication/285775211_Eutrophication_and_hypoxia_in_coastal_areas_a_global_assessment_of_the_state_of_knowledge',
-  'https://www.theguardian.com/commentisfree/2016/aug/09/vegan-corrupt-food-system-meat-dairy',
-  'https://www.theguardian.com/commentisfree/2015/dec/22/festive-christmas-meal-long-haul-flight-meats-damaging-planet',
+  "https://en.wikipedia.org/wiki/Animal_consciousness#Cambridge_Declaration_on_Consciousness",
+  "https://www.theguardian.com/news/2018/may/07/true-cost-of-eating-meat-environment-health-animal-welfare",
+  "http://www.hannejohansen.no/2017/02/12/hvorfor-veganer/",
+  "http://www.hannejohansen.no",
+  "https://forskning.no/2018/05/kjottreduksjon-kan-radikalt-oke-norsk-selvforsyning-av-mat/produsert-og-finansiert-av/nordlandsforskning",
+  "https://forskning.no/mat-landbruk/2018/03/hvis-alle-blir-veganere-vil-verden-fa-mye-mer-mat-kan-lose-matkrise-ved-bytte",
+  "http://waterfootprint.org/en/water-footprint/product-water-footprint/water-footprint-crop-and-animal-products/",
+  "https://www.sciencedirect.com/science/article/pii/S2212371713000024",
+  "http://www.oecd.org/agriculture/water-use-in-agriculture.htm",
+  "https://www.researchgate.net/publication/285775211_Eutrophication_and_hypoxia_in_coastal_areas_a_global_assessment_of_the_state_of_knowledge",
+  "https://www.theguardian.com/commentisfree/2016/aug/09/vegan-corrupt-food-system-meat-dairy",
+  "https://www.theguardian.com/commentisfree/2015/dec/22/festive-christmas-meal-long-haul-flight-meats-damaging-planet"
 ];
 
 const visitedPages = [
-  'https://www.theguardian.com/commentisfree/2016/aug/09/vegan-corrupt-food-system-meat-dairy',
+  "https://www.theguardian.com/commentisfree/2016/aug/09/vegan-corrupt-food-system-meat-dairy"
 ];
 
 const blacklistWords = [
-  'twitter',
-  'facebook',
-  'google',
-  'linkedin',
-  'mailto:',
-  'whatsapp',
-  'pinterest',
-  'fb-messenger'
+  "twitter",
+  "facebook",
+  "google",
+  "linkedin",
+  "mailto:",
+  "whatsapp",
+  "pinterest",
+  "fb-messenger"
 ];
 
-const whitelistInclude = [
-  'vegan',
-  'vegetarian',
-  'meat',
-];
+const whitelistInclude = ["vegan", "vegetarian", "meat"];
 
 const visitedDictionary = {};
 
@@ -62,23 +58,26 @@ const c = new Crawler({
 
       const links = cheerio("a");
       cheerio(links).each((i, link) => {
-        let href = cheerio(link).attr('href');
+        let href = cheerio(link).attr("href");
 
         if (
           href &&
-          (isIncludedInList(href, whitelistInclude) || isIncludedInList(cheerio(link).text(), whitelistInclude)) &&
+          (isIncludedInList(href, whitelistInclude) ||
+            isIncludedInList(cheerio(link).text(), whitelistInclude)) &&
           !isIncludedInList(href, blacklistWords)
         ) {
-          const hrefUrl = href.startsWith('/') ? URL.resolve(res.request.uri.href, href) : href;
+          const hrefUrl = href.startsWith("/")
+            ? URL.resolve(res.request.uri.href, href)
+            : href;
           if (!visitedPages.includes(hrefUrl)) {
             if (visitedPages.length % 100 === 0) {
-              console.log(visitedPages.length + ' entries ..');
+              console.log(visitedPages.length + " entries ..");
             }
 
             visitedPages.push(hrefUrl);
 
             if (visitedDictionary[res.request.uri.hostname]) {
-              visitedDictionary[res.request.uri.hostname].push(hrefUrl)
+              visitedDictionary[res.request.uri.hostname].push(hrefUrl);
             } else {
               visitedDictionary[res.request.uri.hostname] = [hrefUrl];
             }
@@ -86,18 +85,23 @@ const c = new Crawler({
             c.queue(hrefUrl);
           }
         }
-      })
+      });
     }
 
     done();
   }
 });
 
-c.queue('https://www.theguardian.com/commentisfree/2016/aug/09/vegan-corrupt-food-system-meat-dairy');
+c.queue(
+  "https://www.theguardian.com/commentisfree/2016/aug/09/vegan-corrupt-food-system-meat-dairy"
+);
 
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   shouldWrite = false;
-  await Promise.all(Object.keys(visitedDictionary).map(site => write(`./urls/${site}.json`, visitedDictionary[site])));
+  await Promise.all(
+    Object.keys(visitedDictionary).map(site =>
+      write(`./urls/${site}.json`, visitedDictionary[site], { spaces: 2 })
+    )
+  );
   process.exit();
 });
-
